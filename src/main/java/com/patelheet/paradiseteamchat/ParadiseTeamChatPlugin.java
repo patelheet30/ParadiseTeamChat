03a7f390c10fd5ca7b88471c889526d84a94debe
Heet Patel <heetkpatel30@gmail.com>
2026-01-07 12:25:29 +0000

    chore: Bump version to 2.0.0 in POM and plugin files

--- src/main/java/com/patelheet/paradiseteamchat/ParadiseTeamChatPlugin.java
+++ src/main/java/com/patelheet/paradiseteamchat/ParadiseTeamChatPlugin.java
@@ -26,7 +26,7 @@ import com.patelheet.paradiseteamchat.managers.RoleManager;
  * for Paper servers
  * 
  * @author Heet Patel (patelheet30)
- * @version 1.5.1
+ * @version 2.0.0
  */
 public class ParadiseTeamChatPlugin extends JavaPlugin {
     private static ParadiseTeamChatPlugin instance;
