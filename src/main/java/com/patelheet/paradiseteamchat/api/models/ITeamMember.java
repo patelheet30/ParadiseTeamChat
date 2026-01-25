cd55309d3b85500a93abab94631bd70ad1be58ad
Heet Patel <heetkpatel30@gmail.com>
2026-01-25 22:57:22 +0000

    feat: Start work on ITeamMember interface for representing team members with read-only access to their data

---
+++ src/main/java/com/patelheet/paradiseteamchat/api/models/ITeamMember.java
@@ -0,0 +1,12 @@
+package com.patelheet.paradiseteamchat.api.models;
+
+/**
+ * Immutable interface repesenting a member of a team.
+ * 
+ * <p>
+ * This interface provides read-only access to member data, linking a player
+ * to a team with join date and role information.
+ */
+public interface ITeamMember {
+
+}
